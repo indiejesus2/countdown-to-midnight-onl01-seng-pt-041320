9107e81b00716537a29c09da4551ceda0852b665
Dan Ste. Marie Jr <dste.mariejr@gmail.com>
2020-04-14 02:25:29 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,6 +7,11 @@ def countdown(count)
   return "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep
-
+def countdown_with_sleep(count)
+  while count > 0
+    puts "#{count} SECOND(S)!"
+    count -= 1
+    sleep 1
+  end
+  return "HAPPY NEW YEAR!"
 end
